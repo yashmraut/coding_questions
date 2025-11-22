5ebe8c1cfa4b99fe3c12b9f84897f93b7ec2990a
yashmraut <yash77raut@gmail.com>
2025-11-23 04:20:16 +0530

    prime_number.py

---
+++ prime_numbers.py
@@ -0,0 +1,16 @@
+def prime(number): 
+    if number <= 1:
+        return("Not Prime")
+    for i in range(2,int(number**0.5)):
+        if number%i == 0:
+            return("Not prime")
+    else:
+        return("Prime")
+            
+number = 2
+print(prime(number))
+
+# print(int(11**0.5))
+
+# for i in range(2,3):
+#     print(i)
